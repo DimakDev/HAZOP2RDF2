22b5bbb38f89e8a58658cb1c33c71c3ac37553ce
X100 <private.dk@icloud.com>
2021-12-30 02:26:17 +0100

    rethinking workbook, new structure

--- pkg/workbook/workbook.go
+++ pkg/workbook/workbook.go
@@ -7,32 +7,23 @@ import (
 )
 
 type Workbook struct {
-    File       *excelize.File
-    Worksheets []*Worksheet
-}
-
-type Worksheet struct {
-    SheetIndex    int
-    SheetName     string
-    NumOfCols     int
-    NumOfRows     int
-    HazopData     *HazopData
-    HazopHeader   *HazopHeader
-    HazopValidity *HazopValidity
+    File              *excelize.File
+    SheetMap          map[int]string
+    HazopData         map[int]*HazopData
+    HazopHeader       map[int]*HazopHeader
+    HazopValidity     map[int]*HazopValidity
+    HazopDataReport   map[int]*Report
+    HazopHeaderReport map[int]*Report
 }
 
 type HazopData struct {
-    Raw          [][]string
     NodeMetadata [][]interface{}
     NodeHazop    [][]interface{}
-    Report       *Report
 }
 
 type HazopHeader struct {
-    Raw          []string
-    NodeMetadata []string
-    NodeHazop    []string
-    Report       *Report
+    NodeMetadata map[int]string
+    NodeHazop    map[int]string
 }
 
 type HazopValidity struct {
@@ -51,56 +42,44 @@ func NewWorkbook(fpath string) (*Workbook, error) {
     if err != nil {
         return nil, fmt.Errorf("Error opening file: %v", err)
     }
-    wb := &Workbook{File: f}
-    wb.initWorksheets()
+
+    wb := &Workbook{
+        File:              f,
+        SheetMap:          map[int]string{},
+        HazopData:         map[int]*HazopData{},
+        HazopHeader:       map[int]*HazopHeader{},
+        HazopValidity:     map[int]*HazopValidity{},
+        HazopDataReport:   map[int]*Report{},
+        HazopHeaderReport: map[int]*Report{},
+    }
+
     if err := wb.readWorkbook(); err != nil {
         return nil, err
     }
-    if err := wb.verifyWorkbook(); err != nil {
-        return nil, err
-    }
+
     return wb, nil
 }
 
-func (wb *Workbook) initWorksheets() {
-    for i, sname := range wb.File.GetSheetMap() {
-        wb.Worksheets = append(wb.Worksheets, &Worksheet{
-            SheetIndex: i,
-            SheetName:  sname,
-            HazopHeader: &HazopHeader{
-                Raw:          make([]string, len(settings.Hazop.Element)),
-                NodeMetadata: make([]string, len(settings.Hazop.Element)),
-                NodeHazop:    make([]string, len(settings.Hazop.Element)),
-                Report:       &Report{},
-            },
-            HazopData: &HazopData{
-                Report: &Report{},
-            },
-            HazopValidity: &HazopValidity{},
-        })
+func (r *Report) newWarning(msg string) {
+    r.Warnings = append(r.Warnings, msg)
+}
+
+func (r *Report) newError(err error) {
+    r.Errors = append(r.Errors, err)
+}
+
+func (r *Report) newInfo(msg string) {
+    r.Info = append(r.Info, msg)
+}
+
+func (h *HazopHeader) newHeader(i, dtype int, coord string) error {
+    switch dtype {
+    case settings.Hazop.DataType.NodeMetadata:
+        h.NodeMetadata[i] = coord
+    case settings.Hazop.DataType.NodeHazop:
+        h.NodeHazop[i] = coord
+    default:
+        return fmt.Errorf("Unknown data type %d", dtype)
     }
-}
-
-func (hd *HazopData) newWarning(msg string) {
-    hd.Report.Warnings = append(hd.Report.Warnings, msg)
-}
-
-func (hd *HazopData) newError(err error) {
-    hd.Report.Errors = append(hd.Report.Errors, err)
-}
-
-func (hd *HazopData) newInfo(msg string) {
-    hd.Report.Info = append(hd.Report.Info, msg)
-}
-
-func (hh *HazopHeader) newWarning(msg string) {
-    hh.Report.Warnings = append(hh.Report.Warnings, msg)
-}
-
-func (hh *HazopHeader) newError(err error) {
-    hh.Report.Errors = append(hh.Report.Errors, err)
-}
-
-func (hh *HazopHeader) newInfo(msg string) {
-    hh.Report.Info = append(hh.Report.Info, msg)
+    return nil
 }
