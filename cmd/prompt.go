40d60c985f27541625e9fec774b5acc916d8beba
X100 <private.dk@icloud.com>
2021-12-31 18:48:00 +0100

    [style]: fix tab indentation

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -22,96 +22,96 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
+    "fmt"
+    "io/ioutil"
+    "path/filepath"
+    "strings"
 
-	"github.com/dimakdev/hazop-formula/pkg/workbook"
-	"github.com/manifoldco/promptui"
-	"github.com/spf13/cobra"
+    "github.com/dimakdev/hazop-formula/pkg/workbook"
+    "github.com/manifoldco/promptui"
+    "github.com/spf13/cobra"
 )
 
 var promptCmd = &cobra.Command{
-	Use:   "prompt",
-	Short: "Import, parse and verify Excel documents",
-	Long:  "Import, parse and verify Excel documents",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := run(); err != nil {
-			cmd.PrintErrln(err)
-		}
-	},
+    Use:   "prompt",
+    Short: "Import, parse and verify Excel documents",
+    Long:  "Import, parse and verify Excel documents",
+    Run: func(cmd *cobra.Command, args []string) {
+        if err := run(); err != nil {
+            cmd.PrintErrln(err)
+        }
+    },
 }
 
 func init() {
-	rootCmd.AddCommand(promptCmd)
+    rootCmd.AddCommand(promptCmd)
 }
 
 type Command struct {
-	Name        string
-	Description string
-	Datapath    string
+    Name        string
+    Description string
+    Datapath    string
 }
 
 func run() error {
-	common := workbook.GetCommon()
+    common := workbook.GetCommon()
 
-	files, err := ioutil.ReadDir(common.DataDir)
-	if err != nil {
-		return fmt.Errorf("Error reading directory: %v", err)
-	}
+    files, err := ioutil.ReadDir(common.DataDir)
+    if err != nil {
+        return fmt.Errorf("Error reading directory: %v", err)
+    }
 
-	var commands []Command
-	for _, f := range files {
-		if strings.HasSuffix(f.Name(), common.DataExt) {
-			datapath := filepath.Join(common.DataDir, f.Name())
-			commands = append(commands,
-				Command{
-					Name:        fmt.Sprintf("`%s`", f.Name()),
-					Description: fmt.Sprintf("Import, parse and verify `%s`", f.Name()),
-					Datapath:    datapath,
-				},
-			)
-		}
-	}
+    var commands []Command
+    for _, f := range files {
+        if strings.HasSuffix(f.Name(), common.DataExt) {
+            datapath := filepath.Join(common.DataDir, f.Name())
+            commands = append(commands,
+                Command{
+                    Name:        fmt.Sprintf("`%s`", f.Name()),
+                    Description: fmt.Sprintf("Import, parse and verify `%s`", f.Name()),
+                    Datapath:    datapath,
+                },
+            )
+        }
+    }
 
-	if len(commands) == 0 {
-		return fmt.Errorf("Directory `%s` is empty, no `%s` file(s) found", common.DataDir, common.DataExt)
-	}
+    if len(commands) == 0 {
+        return fmt.Errorf("Directory `%s` is empty, no `%s` file(s) found", common.DataDir, common.DataExt)
+    }
 
-	templates := &promptui.SelectTemplates{
-		Label:    "========== {{ . }} ==========",
-		Active:   "⍈ {{ .Name }}",
-		Inactive: "  {{ .Name }}",
-		Selected: "⍈ {{ .Name }}",
-		Details:  "---------- Description -------\n{{ .Description | faint }}",
-	}
+    templates := &promptui.SelectTemplates{
+        Label:    "========== {{ . }} ==========",
+        Active:   "⍈ {{ .Name }}",
+        Inactive: "  {{ .Name }}",
+        Selected: "⍈ {{ .Name }}",
+        Details:  "---------- Description -------\n{{ .Description | faint }}",
+    }
 
-	searcher := func(input string, index int) bool {
-		command := commands[index]
-		name := strings.Replace(strings.ToLower(command.Name), " ", "", -1)
-		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+    searcher := func(input string, index int) bool {
+        command := commands[index]
+        name := strings.Replace(strings.ToLower(command.Name), " ", "", -1)
+        input = strings.Replace(strings.ToLower(input), " ", "", -1)
 
-		return strings.Contains(name, input)
-	}
+        return strings.Contains(name, input)
+    }
 
-	prompt := promptui.Select{
-		Label:     "Commands",
-		Items:     commands,
-		Templates: templates,
-		Size:      8,
-		Searcher:  searcher,
-	}
+    prompt := promptui.Select{
+        Label:     "Commands",
+        Items:     commands,
+        Templates: templates,
+        Size:      8,
+        Searcher:  searcher,
+    }
 
-	i, _, err := prompt.Run()
-	if err != nil {
-		return fmt.Errorf("Prompt failed: %v", err)
-	}
+    i, _, err := prompt.Run()
+    if err != nil {
+        return fmt.Errorf("Prompt failed: %v", err)
+    }
 
-	_, err = workbook.NewWorkbook(commands[i].Datapath)
-	if err != nil {
-		return err
-	}
+    _, err = workbook.NewWorkbook(commands[i].Datapath)
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
