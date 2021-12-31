910cf4bd2c67e736ff6fa6a25ae03ef14f3abaf3
X100 <private.dk@icloud.com>
2021-12-31 05:27:24 +0100

    update settings structure, add some getter

--- pkg/workbook/settings.go
+++ pkg/workbook/settings.go
@@ -7,37 +7,49 @@ import (
 )
 
 type Settings struct {
-    Package struct {
-        Name        string `mapstructure:"name"`
-        Description string `mapstructure:"description"`
-        Help        string `mapstructure:"help"`
-        Version     string `mapstructure:"version"`
-        Author      string `mapstructure:"author"`
-    } `mapstructure:"package"`
-    Common struct {
-        DataDir   string `mapstructure:"data_dir"`
-        DataExt   string `mapstructure:"data_ext"`
-        ReportDir string `mapstructure:"report_dir"`
-    } `mapstructure:"common"`
-    Hazop struct {
-        DataType struct {
-            NodeMetadata int `mapstructure:"node_metadata"`
-            NodeHazop    int `mapstructure:"node_hazop"`
-        } `mapstructure:"data_type"`
-        CellType struct {
-            String  int `mapstructure:"string"`
-            Integer int `mapstructure:"integer"`
-            Float   int `mapstructure:"float"`
-        } `mapstructure:"cell_type"`
-        Element []struct {
-            DataType int    `mapstructure:"data_type"`
-            Name     string `mapstructure:"name"`
-            Regex    string `mapstructure:"regex"`
-            CellType int    `mapstructure:"cell_type"`
-            MinLen   int    `mapstructure:"min_len"`
-            MaxLen   int    `mapstructure:"max_len"`
-        } `mapstructure:"element"`
-    } `mapstructure:"hazop"`
+    Package Package `mapstructure:"package"`
+    Common  Common  `mapstructure:"common"`
+    Hazop   Hazop   `mapstructure:"hazop"`
+}
+
+type Package struct {
+    Name        string `mapstructure:"name"`
+    Description string `mapstructure:"description"`
+    Help        string `mapstructure:"help"`
+    Version     string `mapstructure:"version"`
+    Author      string `mapstructure:"author"`
+}
+
+type Common struct {
+    DataDir   string `mapstructure:"data_dir"`
+    DataExt   string `mapstructure:"data_ext"`
+    ReportDir string `mapstructure:"report_dir"`
+}
+
+type DataType struct {
+    Metadata int `mapstructure:"metadata"`
+    Analysis int `mapstructure:"analysis"`
+}
+
+type CellType struct {
+    String  int `mapstructure:"string"`
+    Integer int `mapstructure:"integer"`
+    Float   int `mapstructure:"float"`
+}
+
+type Element struct {
+    DataType int    `mapstructure:"data_type"`
+    Name     string `mapstructure:"name"`
+    Regex    string `mapstructure:"regex"`
+    CellType int    `mapstructure:"cell_type"`
+    MinLen   int    `mapstructure:"min_len"`
+    MaxLen   int    `mapstructure:"max_len"`
+}
+
+type Hazop struct {
+    DataType DataType  `mapstructure:"data_type"`
+    CellType CellType  `mapstructure:"cell_type"`
+    Element  []Element `mapstructure:"element"`
 }
 
 var settings Settings
@@ -54,4 +66,19 @@ func init() {
     if err := viper.Unmarshal(&settings); err != nil {
         log.Fatal("Error parsing `.toml` settings: ", err)
     }
+}
+
+func groupElements(dtype int) map[int]Element {
+    elements := map[int]Element{}
+    for i, e := range settings.Hazop.Element {
+        if e.DataType != dtype {
+            continue
+        }
+        elements[i] = e
+    }
+    return elements
+}
+
+func getElement(idx int) Element {
+    return settings.Hazop.Element[idx]
 }
