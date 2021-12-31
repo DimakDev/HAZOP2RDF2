8bfa1ec02b1ff1499f05920f33daba3215058c76
X100 <private.dk@icloud.com>
2021-12-31 18:42:58 +0100

    [prompt]: cut all unnecessary commands

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -1,155 +1,117 @@
-package main
+/*
+Copyright © 2021 Dmytro Kostiuk <X100@X100.LINK>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "path/filepath"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
 
-    "github.com/manifoldco/promptui"
+	"github.com/dimakdev/hazop-formula/pkg/workbook"
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
 )
 
-type Alias string
+var promptCmd = &cobra.Command{
+	Use:   "prompt",
+	Short: "Import, parse and verify Excel documents",
+	Long:  "Import, parse and verify Excel documents",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := run(); err != nil {
+			cmd.PrintErrln(err)
+		}
+	},
+}
 
-const (
-    CMD_HEAD  Alias = "CMD_HEAD"
-    CMD_TAIL        = "CMD_TAIL"
-    CMD_FULL        = "CMD_FULL"
-    CMD_INPUT       = "CMD_INPUT"
-    CMD_DATA        = "CMD_DATA"
-    CMD_GRAPH       = "CMD_GRAPH"
-)
+func init() {
+	rootCmd.AddCommand(promptCmd)
+}
 
 type Command struct {
-    Name  string
-    Alias Alias
-    Desc  string
-    File  string
+	Name        string
+	Description string
+	Datapath    string
 }
 
-type Prompt struct {
-    PromptUI promptui.Select
-    Commands []Command
-}
+func run() error {
+	common := workbook.GetCommon()
 
-func main() {
-    prompt := &Prompt{}
-    if err := prompt.generate(); err != nil {
-        log.Fatal(err)
-    }
-    if err := prompt.run(); err != nil {
-        log.Fatal(err)
-    }
-}
+	files, err := ioutil.ReadDir(common.DataDir)
+	if err != nil {
+		return fmt.Errorf("Error reading directory: %v", err)
+	}
 
-func (p *Prompt) generate() error {
-    files, err := ioutil.ReadDir(datadir)
-    if err != nil {
-        return fmt.Errorf("Error reading `%s`: %v", datadir, err)
-    }
+	var commands []Command
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), common.DataExt) {
+			datapath := filepath.Join(common.DataDir, f.Name())
+			commands = append(commands,
+				Command{
+					Name:        fmt.Sprintf("`%s`", f.Name()),
+					Description: fmt.Sprintf("Import, parse and verify `%s`", f.Name()),
+					Datapath:    datapath,
+				},
+			)
+		}
+	}
 
-    var commands []Command
-    for _, f := range files {
-        if strings.HasSuffix(f.Name(), dataext) {
-            datapath := filepath.Join(datadir, f.Name())
-            commands = append(commands,
-                Command{
-                    Name:  fmt.Sprintf("Head `%s`", f.Name()),
-                    Alias: CMD_HEAD,
-                    Desc:  fmt.Sprintf("Show first rows `%s`", f.Name()),
-                    File:  datapath,
-                },
-                Command{
-                    Name:  fmt.Sprintf("Tail `%s`", f.Name()),
-                    Alias: CMD_TAIL,
-                    Desc:  fmt.Sprintf("Show last rows `%s`", f.Name()),
-                    File:  datapath,
-                },
-                Command{
-                    Name:  fmt.Sprintf("Show full `%s`", f.Name()),
-                    Alias: CMD_FULL,
-                    Desc:  fmt.Sprintf("Show all rows `%s`", f.Name()),
-                    File:  datapath,
-                },
-                Command{
-                    Name:  fmt.Sprintf("Verificate input `%s`", f.Name()),
-                    Alias: CMD_INPUT,
-                    Desc:  fmt.Sprintf("Verificate input `%s`:\n  1. Check column name (regex, synonyms).\n  2. Check column type (number, text).\nVerification results will be saved in `%s`.", f.Name(), logpath),
-                    File:  datapath,
-                },
-                Command{
-                    Name:  fmt.Sprintf("Verificate data `%s`", f.Name()),
-                    Alias: CMD_DATA,
-                    Desc:  fmt.Sprintf("Verificate data `%s`:\n  1. Check value length.\n  2. Check value range.\n  3. Check optional and mandatory fields.\nVerification results will be written in `%s`.", f.Name(), logpath),
-                    File:  datapath,
-                },
-                Command{
-                    Name:  fmt.Sprintf("Create Hazop graph `%s`", f.Name()),
-                    Alias: CMD_GRAPH,
-                    Desc:  fmt.Sprintf("Create RDF graph from source `%s`", f.Name()),
-                    File:  datapath,
-                },
-            )
-        }
-    }
+	if len(commands) == 0 {
+		return fmt.Errorf("Directory `%s` is empty, no `%s` file(s) found", common.DataDir, common.DataExt)
+	}
 
-    if len(commands) == 0 {
-        return fmt.Errorf("`%s` is empty, no `%s` file(s) found", datadir, dataext)
-    }
+	templates := &promptui.SelectTemplates{
+		Label:    "========== {{ . }} ==========",
+		Active:   "⍈ {{ .Name }}",
+		Inactive: "  {{ .Name }}",
+		Selected: "⍈ {{ .Name }}",
+		Details:  "---------- Description -------\n{{ .Description | faint }}",
+	}
 
-    templates := &promptui.SelectTemplates{
-        Label:    "========== {{ . }} ==========",
-        Active:   "➡️ {{ .Name | cyan }}",
-        Inactive: "  {{ .Name | cyan }}",
-        Selected: "➡️ {{ .Name | red }}",
-        Details:  "---------- Description -------------\n{{ .Desc | faint }}",
-    }
+	searcher := func(input string, index int) bool {
+		command := commands[index]
+		name := strings.Replace(strings.ToLower(command.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
 
-    searcher := func(input string, index int) bool {
-        command := commands[index]
-        name := strings.Replace(strings.ToLower(command.Name), " ", "", -1)
-        input = strings.Replace(strings.ToLower(input), " ", "", -1)
+		return strings.Contains(name, input)
+	}
 
-        return strings.Contains(name, input)
-    }
+	prompt := promptui.Select{
+		Label:     "Commands",
+		Items:     commands,
+		Templates: templates,
+		Size:      8,
+		Searcher:  searcher,
+	}
 
-    p.PromptUI = promptui.Select{
-        Label:     "Select Command",
-        Items:     commands,
-        Templates: templates,
-        Size:      8,
-        Searcher:  searcher,
-    }
+	i, _, err := prompt.Run()
+	if err != nil {
+		return fmt.Errorf("Prompt failed: %v", err)
+	}
 
-    p.Commands = commands
+	_, err = workbook.NewWorkbook(commands[i].Datapath)
+	if err != nil {
+		return err
+	}
 
-    return nil
-}
-
-func (p *Prompt) run() error {
-    for {
-        i, _, err := p.PromptUI.Run()
-        if err != nil {
-            return fmt.Errorf("Prompt failed: %v", err)
-        }
-
-        switch p.Commands[i].Alias {
-        case CMD_HEAD:
-            log.Println(CMD_HEAD)
-        case CMD_TAIL:
-            log.Println(CMD_TAIL)
-        case CMD_FULL:
-            log.Println(CMD_FULL)
-        case CMD_INPUT:
-            log.Println(CMD_INPUT)
-        case CMD_DATA:
-            log.Println(CMD_DATA)
-        case CMD_GRAPH:
-            log.Println(CMD_GRAPH)
-        default:
-            return fmt.Errorf("Error alias: `%s` not found", p.Commands[i].Alias)
-        }
-    }
-    return nil
+	return nil
 }
