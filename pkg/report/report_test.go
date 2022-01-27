cdafafd794b2283ec165528dcd8831ab9a797208
X100 <private.dk@icloud.com>
2022-01-27 17:05:17 +0100

    add report tests

---
+++ pkg/report/report_test.go
@@ -0,0 +1,47 @@
+package report
+
+import (
+    "os"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func TestReportToFile(t *testing.T) {
+    assert := assert.New(t)
+
+    rpath := "report.txt"
+    tpath := "template_file.txt"
+
+    report := new(Report)
+
+    var err error
+
+    err = report.ReportToFile("", "")
+    assert.Error(err)
+
+    err = report.ReportToFile(rpath, "")
+    assert.Error(err)
+
+    err = report.ReportToFile(rpath, tpath)
+    assert.Empty(err)
+
+    err = os.Remove(rpath)
+    assert.Empty(err)
+}
+
+func TestReportToStdout(t *testing.T) {
+    assert := assert.New(t)
+
+    tpath := "template_stdout.txt"
+
+    report := new(Report)
+
+    var err error
+
+    err = report.ReportToStdout("")
+    assert.Error(err)
+
+    err = report.ReportToStdout(tpath)
+    assert.Empty(err)
+}
