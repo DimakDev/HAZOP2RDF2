574b3bbac178d37d18de92a09dbdd93f8e4bd095
X100 <private.dk@icloud.com>
2022-01-28 03:14:26 +0100

    add workbook benchmarks and tests

--- pkg/workbook/workbook_test.go
+++ pkg/workbook/workbook_test.go
@@ -1,23 +1,57 @@
 package workbook
 
 import (
+    "fmt"
     "io/ioutil"
+    "log"
     "path/filepath"
     "strings"
     "sync"
     "testing"
+
+    "github.com/stretchr/testify/assert"
 )
 
 func BenchmarkReadVerifyWorkbook(b *testing.B) {
-    files, _ := ioutil.ReadDir("data")
+    files, err := ioutil.ReadDir("data")
+    if err != nil {
+        log.Fatal(err)
+    }
 
     var wg sync.WaitGroup
-    for i := 0; i < b.N; i++ {
-        for _, file := range files {
-            if strings.HasSuffix(file.Name(), ".xlsx") {
-                fpath := filepath.Join("data", file.Name())
-                ReadVerifyWorkbook(fpath, &wg)
-            }
+    for _, file := range files {
+        if strings.HasSuffix(file.Name(), ".xlsx") {
+            fpath := filepath.Join("data", file.Name())
+            b.Run(fmt.Sprintf("Workbook: %s\n", fpath), func(b *testing.B) {
+                for i := 0; i < b.N; i++ {
+                    ReadVerifyWorkbook(fpath, &wg)
+                }
+            })
+        }
+    }
+
+    wg.Wait()
+}
+
+func TestReadVerifyWorkbook(t *testing.T) {
+    assert := assert.New(t)
+
+    var wg sync.WaitGroup
+
+    wb, err := ReadVerifyWorkbook("", &wg)
+    assert.Error(err)
+    assert.Empty(wb)
+
+    files, err := ioutil.ReadDir("data")
+    assert.Empty(err)
+    assert.NotEmpty(files)
+
+    for _, file := range files {
+        if strings.HasSuffix(file.Name(), ".xlsx") {
+            fpath := filepath.Join("data", file.Name())
+            wg, err := ReadVerifyWorkbook(fpath, &wg)
+            assert.Empty(err)
+            assert.NotEmpty(wg)
         }
     }
 
